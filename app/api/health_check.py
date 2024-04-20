4f767081ea66689ac98e0bc4209fec23d6f91c17
Sergei Svechnikov <srgcandle@mail.ru>
2024-04-20 18:18:54 +0400

    refactor(api): add docstrings

--- app/api/health_check.py
+++ app/api/health_check.py
@@ -1,3 +1,5 @@
+"""Simple route to check application viability."""
+
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,4 +7,10 @@ router = APIRouter()
 
 @router.get("/health-check")
 def health_check() -> dict[str, str]:
+    """Checks if application is alive.
+
+    Returns:
+        Simple dict if alive
+
+    """
     return {"ping": "pong"}
--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+"""Entrypoint to application."""
+
 from fastapi import FastAPI
 
 from app.api import health_check
 
 
 def create_application() -> FastAPI:
+    """Creates root application.
+
+    Collects all routes
+
+    Returns:
+        Ready to run application
+
+    """
     application = FastAPI()
     application.include_router(router=health_check.router, tags=["health-check"])
 
